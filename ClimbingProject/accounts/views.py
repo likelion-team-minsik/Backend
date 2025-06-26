9a0e68eb8b6b998f9e45faedaccf4bfebf4b20ec
anemic-girl <rlaalsrl-030404@naver.com>
2025-06-26 20:41:10 +0900

    회원가입, 로그인, 마이페이지, 검색

---
+++ ClimbingProject/accounts/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import viewsets
+from .serializers import UserSerializer
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .serializers import UserProfileSerializer
+
+
+User = get_user_model()                   
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()            # 커스텀 User 레코드 전체
+    serializer_class = UserSerializer
+
+class UserProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        serializer = UserProfileSerializer(request.user)
+        return Response(serializer.data)
+
+    def put(self, request):
+        serializer = UserProfileSerializer(request.user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
